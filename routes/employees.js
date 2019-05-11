63a71ef7df95595370e42f40bb79d7c8fc0a1699
jetmech <josephlesage84@gmail.com>
2019-05-11 15:53:52 +1000

    Implement "GET /" path

--- routes/employees.js
+++ routes/employees.js
@@ -1,5 +1,15 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const Employee = require('../models/employee');
 const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const employees = await Employee.getCurrentlyEmployed();
+    return res.send({ employees: employees });
+  } catch (error) {
+    return res.status(500).send();
+  }
+});
 
 module.exports = router;
