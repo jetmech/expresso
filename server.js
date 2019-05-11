d9bf901b8c6a938dbc5aec22856e16b5775c6ab3
jetmech <josephlesage84@gmail.com>
2019-05-11 15:54:21 +1000

    Add "/api/employees/" path

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const employeesRouter = require('./routes/employees');
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+app.use('/api/employees/', employeesRouter);
 
 const server = app.listen(PORT);
 
