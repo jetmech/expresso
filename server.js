460b287937b493804227409d7bf683ea85568484
jetmech <josephlesage84@gmail.com>
2019-05-17 15:05:01 +0800

    Add morgan and cors

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const employeesRouter = require('./routes/employees');
 const menusRouter = require('./routes/menus');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(morgan('short'));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/employees/', employeesRouter);
