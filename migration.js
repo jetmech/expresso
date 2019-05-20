d896616f5e0545e170127b5f0093ba4af206f117
jetmech <josephlesage84@gmail.com>
2019-05-20 20:11:32 +1000

    Serialise table creation

--- migration.js
+++ migration.js
@@ -9,38 +9,41 @@ const db = new sqlite3.Database('database.sqlite', (err) => {
   }
 });
 
-db.exec(`CREATE TABLE IF NOT EXISTS "Employee" (
-	"id"	INTEGER,
-	"name"	TEXT NOT NULL,
-	"position"	TEXT NOT NULL,
-	"wage"	TEXT NOT NULL,
-	"is_current_employee"	INTEGER DEFAULT 1,
-	PRIMARY KEY("id")
-);`);
+db.serialize(function() {
 
-db.exec(`CREATE TABLE IF NOT EXISTS "Timesheet" (
-  "id" INTEGER,
-  "hours" INTEGER NOT NULL,
-  "rate" INTEGER NOT NULL,
-  "date" INTEGER NOT NULL,
-  "employee_id" INTEGER NOT NULL,
-  FOREIGN KEY("employee_id") REFERENCES "Employee"("id"),
-  PRIMARY KEY("id")
-);`);
-
-db.exec(`CREATE TABLE IF NOT EXISTS "Menu" (
-  "id" INTEGER,
-  "title" INTEGER NOT NULL,
-  PRIMARY KEY("id")
-);`);
-
-db.exec(`CREATE TABLE IF NOT EXISTS "MenuItem" (
-  "id" INTEGER,
-  "name" TEXT NOT NULL,
-  "description" TEXT,
-  "inventory" INTEGER NOT NULL,
-  "price" INTEGER NOT NULL,
-  "menu_id" INTEGER NOT NULL,
-	FOREIGN KEY("menu_id") REFERENCES "Menu"("id"),
-	PRIMARY KEY("id")
-);`);
+  db.exec(`CREATE TABLE IF NOT EXISTS "Employee" (
+    "id"	INTEGER,
+    "name"	TEXT NOT NULL,
+    "position"	TEXT NOT NULL,
+    "wage"	TEXT NOT NULL,
+    "is_current_employee"	INTEGER DEFAULT 1,
+    PRIMARY KEY("id")
+  );`);
+  
+  db.exec(`CREATE TABLE IF NOT EXISTS "Timesheet" (
+    "id" INTEGER,
+    "hours" INTEGER NOT NULL,
+    "rate" INTEGER NOT NULL,
+    "date" INTEGER NOT NULL,
+    "employee_id" INTEGER NOT NULL,
+    FOREIGN KEY("employee_id") REFERENCES "Employee"("id"),
+    PRIMARY KEY("id")
+  );`);
+  
+  db.exec(`CREATE TABLE IF NOT EXISTS "Menu" (
+    "id" INTEGER,
+    "title" INTEGER NOT NULL,
+    PRIMARY KEY("id")
+  );`);
+  
+  db.exec(`CREATE TABLE IF NOT EXISTS "MenuItem" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "inventory" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    "menu_id" INTEGER NOT NULL,
+    FOREIGN KEY("menu_id") REFERENCES "Menu"("id"),
+    PRIMARY KEY("id")
+  );`);
+});
